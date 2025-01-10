cbecb22b9ba930e238fa010a1f1f78a3901910b4
Jacob Glanz <jacobg@goflow.com>
2025-01-10 10:55:56 -0500

    Add Users to DB

--- RazorMass/DB.cs
+++ RazorMass/DB.cs
@@ -2,4 +2,13 @@ namespace RazorMass;
 
 public static class DB
 {
+    public static List<User> Users { get; } = [];
+}
+
+public class User(string firstName, string lastName, string email, string password)
+{
+    public string FirstName { get; set; } = firstName;
+    public string LastName { get; set; } = lastName;
+    public string Email { get; set; } = email;
+    public string Password { get; set; } = password;
 }
