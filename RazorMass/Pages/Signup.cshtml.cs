1c836f51eeca007f7fbefcf8fa92f4bc418dfb06
Jacob Glanz <jacobg@goflow.com>
2025-01-11 20:33:40 -0500

    WIP

---
+++ RazorMass/Pages/Signup.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorMass.Pages;
+
+public class SignupModel : PageModel
+{
+    public void OnGetAsync() { }
+}
